b676144e7685ba1b2e7f4585e1fd98b020cba0f1
Amer Elsayed Shaat <58009427+AmerShaat25@users.noreply.github.com>
2023-07-19 16:34:29 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,8 @@
  * main
  *
  */
-
 int main (void)
 {
-	puts ("Programming is like building a multilingual puzzle");
-		return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return(0);
 }
