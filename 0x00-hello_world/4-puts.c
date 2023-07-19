b1435dbd362314334ec86a6af882dae6a44747e4
Amer Elsayed Shaat <58009427+AmerShaat25@users.noreply.github.com>
2023-07-19 17:04:57 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /*
- * main
+ * main func is a entry point in c language
  */
-int main()
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle,/n");
-	return(0);
+	
 }
