6acbd7b96c0c124e89a5f658b789103fcaec9db1
Amer Elsayed Shaat <58009427+AmerShaat25@users.noreply.github.com>
2023-07-19 17:25:14 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main func is a entry point in c language
- *return always  0
- */
-int main (void)
+* main - Entry point 
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle/n");
-		return (0);
+	puts("\"Programming is like building a multilingual puzzle");	
+	return (0);
 }
